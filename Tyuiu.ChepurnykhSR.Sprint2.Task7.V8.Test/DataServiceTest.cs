1363b8bccb7aa58cffa4eff2ac60a932d4b82278
Tyuiu.ChepurnykhSR@outlook.com <winxsofa@bk.ru>
2023-10-14 21:12:01 +0500

    Task7

---
+++ Tyuiu.ChepurnykhSR.Sprint2.Task7.V8.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ChepurnykhSR.Sprint2.Task7.V8.Lib;
+
+namespace Tyuiu.ChepurnykhSR.Sprint2.Task7.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+            int x = 3;
+            int y = 1;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
